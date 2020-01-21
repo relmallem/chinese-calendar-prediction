57be384eae61a7ff6cf0b6169434db534a3ee80f
Rachid El Mallem <60019623+relmallem@users.noreply.github.com>
2020-01-21 20:40:16 +0100

    Add files via upload

--- predicteur.py
+++ predicteur.py
@@ -1,31 +1,31 @@
-Matrice = ("Fille","Garçon","Fille","Garçon","Garçon","Garçon","Garçon","Garçon","Garçon","Garçon","Garçon","Garçon",
-           "Garçon","Fille","Garçon","Fille","Fille","Garçon","Garçon","Fille","Garçon","Garçon","Fille","Fille",
-           "Fille","Garçon","Fille","Garçon","Garçon","Garçon","Garçon","Garçon","Garçon","Fille","Garçon","Garçon",
-           "Garçon","Fille","Fille","Fille","Fille","Fille","Fille","Fille","Fille","Fille","Fille","Fille",
-           "Fille","Garçon","Garçon","Fille","Garçon","Fille","Fille","Garçon","Fille","Fille","Fille","Fille",
-           "Garçon","Garçon","Garçon","Fille","Garçon","Garçon","Fille","Fille","Fille","Garçon","Garçon","Fille",
-           "Garçon","Fille","Fille","Garçon","Garçon","Fille","Garçon","Fille","Garçon","Garçon","Fille","Garçon",
-           "Fille","Garçon","Fille","Garçon","Fille","Garçon","Fille","Garçon","Fille","Garçon","Garçon","Garçon",
-           "Garçon","Garçon","Garçon","Garçon","Garçon","Fille","Garçon","Fille","Fille","Garçon","Fille","Fille",
-           "Fille","Fille","Garçon","Garçon","Fille","Garçon","Fille","Fille","Garçon","Fille","Garçon","Garçon",
-           "Garçon","Garçon","Garçon","Fille","Fille","Garçon","Fille","Garçon","Fille","Fille","Garçon","Fille",
-           "Fille","Garçon","Fille","Fille","Garçon","Fille","Garçon","Garçon","Fille","Garçon","Fille","Fille",
-           "Garçon","Garçon","Fille","Garçon","Fille","Garçon","Garçon","Garçon","Garçon","Garçon","Garçon","Garçon",
-           "Garçon","Garçon","Garçon","Garçon","Fille","Fille","Garçon","Fille","Garçon","Fille","Fille","Fille",
-           "Garçon","Fille","Fille","Garçon","Fille","Garçon","Garçon","Fille","Garçon","Garçon","Fille","Garçon",
-           "Fille","Garçon","Garçon","Fille","Fille","Garçon","Fille","Garçon","Fille","Garçon","Garçon","Fille",
-           "Garçon","Garçon","Fille","Fille","Garçon","Fille","Garçon","Garçon","Fille","Garçon","Fille","Fille",
-           "Garçon","Fille","Garçon","Fille","Garçon","Fille","Garçon","Fille","Garçon","Garçon","Fille","Garçon",
-           "Garçon","Fille","Garçon","Garçon","Garçon","Fille","Garçon","Garçon","Fille","Fille","Fille","Fille",
-           "Fille","Fille","Garçon","Fille","Fille","Fille","Garçon","Fille","Fille","Garçon","Garçon","Garçon",
-           "Garçon","Garçon","Fille","Fille","Garçon","Fille","Fille","Garçon","Fille","Fille","Garçon","Fille",
-           "Fille","Fille","Garçon","Fille","Fille","Fille","Garçon","Fille","Garçon","Garçon","Fille","Garçon",
-           "Garçon","Garçon","Garçon","Fille","Garçon","Fille","Garçon","Fille","Garçon","Fille","Fille","Garçon",
-           "Fille","Fille","Garçon","Fille","Garçon","Garçon","Fille","Fille","Garçon","Fille","Garçon","Fille",
-           "Garçon","Fille","Fille","Garçon","Garçon","Garçon","Garçon","Garçon","Fille","Garçon","Fille","Garçon",
-           "Fille","Garçon","Fille","Fille","Garçon","Garçon","Garçon","Fille","Fille","Fille","Garçon","Garçon",
-           "Garçon","Fille","Fille","Fille","Garçon","Fille","Garçon","Garçon","Fille","Garçon","Fille","Garçon",
-           "Fille","Garçon","Fille","Garçon","Fille","Fille","Garçon","Fille","Garçon","Fille","Garçon","Fille")
+Matrice = ("Une Fille","Un Garçon","Une Fille","Un Garçon","Un Garçon","Un Garçon","Un Garçon","Un Garçon","Un Garçon","Un Garçon","Un Garçon","Un Garçon",
+           "Un Garçon","Une Fille","Un Garçon","Une Fille","Une Fille","Un Garçon","Un Garçon","Une Fille","Un Garçon","Un Garçon","Une Fille","Une Fille",
+           "Une Fille","Un Garçon","Une Fille","Un Garçon","Un Garçon","Un Garçon","Un Garçon","Un Garçon","Un Garçon","Une Fille","Un Garçon","Un Garçon",
+           "Un Garçon","Une Fille","Une Fille","Une Fille","Une Fille","Une Fille","Une Fille","Une Fille","Une Fille","Une Fille","Une Fille","Une Fille",
+           "Une Fille","Un Garçon","Un Garçon","Une Fille","Un Garçon","Une Fille","Une Fille","Un Garçon","Une Fille","Une Fille","Une Fille","Une Fille",
+           "Un Garçon","Un Garçon","Un Garçon","Une Fille","Un Garçon","Un Garçon","Une Fille","Une Fille","Une Fille","Un Garçon","Un Garçon","Une Fille",
+           "Un Garçon","Une Fille","Une Fille","Un Garçon","Un Garçon","Une Fille","Un Garçon","Une Fille","Un Garçon","Un Garçon","Une Fille","Un Garçon",
+           "Une Fille","Un Garçon","Une Fille","Un Garçon","Une Fille","Un Garçon","Une Fille","Un Garçon","Une Fille","Un Garçon","Un Garçon","Un Garçon",
+           "Un Garçon","Un Garçon","Un Garçon","Un Garçon","Un Garçon","Une Fille","Un Garçon","Une Fille","Une Fille","Un Garçon","Une Fille","Une Fille",
+           "Une Fille","Une Fille","Un Garçon","Un Garçon","Une Fille","Un Garçon","Une Fille","Une Fille","Un Garçon","Une Fille","Un Garçon","Un Garçon",
+           "Un Garçon","Un Garçon","Un Garçon","Une Fille","Une Fille","Un Garçon","Une Fille","Un Garçon","Une Fille","Une Fille","Un Garçon","Une Fille",
+           "Une Fille","Un Garçon","Une Fille","Une Fille","Un Garçon","Une Fille","Un Garçon","Un Garçon","Une Fille","Un Garçon","Une Fille","Une Fille",
+           "Un Garçon","Un Garçon","Une Fille","Un Garçon","Une Fille","Un Garçon","Un Garçon","Un Garçon","Un Garçon","Un Garçon","Un Garçon","Un Garçon",
+           "Un Garçon","Un Garçon","Un Garçon","Un Garçon","Une Fille","Une Fille","Un Garçon","Une Fille","Un Garçon","Une Fille","Une Fille","Une Fille",
+           "Un Garçon","Une Fille","Une Fille","Un Garçon","Une Fille","Un Garçon","Un Garçon","Une Fille","Un Garçon","Un Garçon","Une Fille","Un Garçon",
+           "Une Fille","Un Garçon","Un Garçon","Une Fille","Une Fille","Un Garçon","Une Fille","Un Garçon","Une Fille","Un Garçon","Un Garçon","Une Fille",
+           "Un Garçon","Un Garçon","Une Fille","Une Fille","Un Garçon","Une Fille","Un Garçon","Un Garçon","Une Fille","Un Garçon","Une Fille","Une Fille",
+           "Un Garçon","Une Fille","Un Garçon","Une Fille","Un Garçon","Une Fille","Un Garçon","Une Fille","Un Garçon","Un Garçon","Une Fille","Un Garçon",
+           "Un Garçon","Une Fille","Un Garçon","Un Garçon","Un Garçon","Une Fille","Un Garçon","Un Garçon","Une Fille","Une Fille","Une Fille","Une Fille",
+           "Une Fille","Une Fille","Un Garçon","Une Fille","Une Fille","Une Fille","Un Garçon","Une Fille","Une Fille","Un Garçon","Un Garçon","Un Garçon",
+           "Un Garçon","Un Garçon","Une Fille","Une Fille","Un Garçon","Une Fille","Une Fille","Un Garçon","Une Fille","Une Fille","Un Garçon","Une Fille",
+           "Une Fille","Une Fille","Un Garçon","Une Fille","Une Fille","Une Fille","Un Garçon","Une Fille","Un Garçon","Un Garçon","Une Fille","Un Garçon",
+           "Un Garçon","Un Garçon","Un Garçon","Une Fille","Un Garçon","Une Fille","Un Garçon","Une Fille","Un Garçon","Une Fille","Une Fille","Un Garçon",
+           "Une Fille","Une Fille","Un Garçon","Une Fille","Un Garçon","Un Garçon","Une Fille","Une Fille","Un Garçon","Une Fille","Un Garçon","Une Fille",
+           "Un Garçon","Une Fille","Une Fille","Un Garçon","Un Garçon","Un Garçon","Un Garçon","Un Garçon","Une Fille","Un Garçon","Une Fille","Un Garçon",
+           "Une Fille","Un Garçon","Une Fille","Une Fille","Un Garçon","Un Garçon","Un Garçon","Une Fille","Une Fille","Une Fille","Un Garçon","Un Garçon",
+           "Un Garçon","Une Fille","Une Fille","Une Fille","Un Garçon","Une Fille","Un Garçon","Un Garçon","Une Fille","Un Garçon","Une Fille","Un Garçon",
+           "Une Fille","Un Garçon","Une Fille","Un Garçon","Une Fille","Une Fille","Un Garçon","Une Fille","Un Garçon","Une Fille","Un Garçon","Une Fille")
 
 while 1 == 1:
     age = int(input("Entrez votre age à la conception : "))
@@ -33,5 +33,6 @@ while 1 == 1:
     if age not in range(18,46) and mois not in range(1,13):
         print("Veuillez indiquer un age compris entre 18 et 45 ans et un mois compris entre 1 et 12")
     else:
-        print("Ce sera un.e "+ Matrice[((age-18)*12)+mois-1])
+        print("Ce sera "+ Matrice[((age-18)*12)+mois-1])
         print("Félicitations")
+
